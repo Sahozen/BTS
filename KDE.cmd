7f54d5a488e3000eede223e636c3597bc3bd2341
Saho <147002941+Sahozen@users.noreply.github.com>
2025-01-07 13:46:17 +0100

    Create KDE.cmd

---
+++ KDE.cmd
@@ -0,0 +1,8 @@
+sudo apt install plasma-workspace plasma-discover -y
+sudo apt install plasma-discover-backend-flatpak plasma-discover-backend-snap -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install kde-cli-tools
+kquitapp5 plasmashell && kstart5 plasmashell
+
+  
