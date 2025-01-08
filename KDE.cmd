8806889165ab88bea91c5386d338018642ebc710
Saho <147002941+Sahozen@users.noreply.github.com>
2025-01-08 09:40:52 +0100

    Update KDE.cmd

--- KDE.cmd
+++ KDE.cmd
@@ -1,3 +1,12 @@
+sudo nano /etc/apt/sources.list
+
+deb http://deb.debian.org/debian/ bookworm main contrib non-free
+deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free
+deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free
+# deb http://security.debian.org/debian-security bookworm-security main contrib non-free
+
+
+
 sudo apt install plasma-workspace plasma-discover -y
 sudo apt install plasma-discover-backend-flatpak plasma-discover-backend-snap -y
 sudo apt update
