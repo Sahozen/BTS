86a3565ec277e84b7905fdc7e970ce7d20e74818
Saho <147002941+Sahozen@users.noreply.github.com>
2025-01-28 11:57:32 +0100

    Update certificatglpi.sh

--- certificatglpi.sh
+++ certificatglpi.sh
@@ -9,3 +9,24 @@ repondre : no
 Redirect permanent / https://glpi.alphatech.local
 
 sudo nano /etc/apache2/sites-available/glpi.alphatech.local.conf
+
+#entre les balise filesmatch et changer le port 80 en port 443 
+
+SSLCertificateFile /etc/letsencrypt/live/glpi.alphatech.local/fullchain.pem
+SSLCertificateKeyFile /etc/letsencrypt/live/glpi.alphatech.local/privkey.pem
+Include /etc/letsencrypt/options-ssl-apache.conf
+Header always set Strict-Transport-Security "max-age=31536000"
+
+sudo systemctl restart apache2
+
+#puis tester le site et lancer une analyse
+
+sudo certbot renew --dry-run
+
+sudo crontab -e
+
+0 5 * * * /usr/bin/certbot renew --quiet
+
+
+
+
