a29f4b71e51bab65e7a38bdad6bc29abd5c8fc9d
Saho <147002941+Sahozen@users.noreply.github.com>
2025-02-07 09:39:53 +0100

    Create Notifglpi.sh

---
+++ Notifglpi.sh
@@ -0,0 +1,12 @@
+sudo chown -R www-data:www-data /etc/glpi/
+
+
+sudo chmod 640 /etc/glpi/config_db.php
+sudo chmod 640 /etc/glpi/glpicrypt.key
+
+
+sudo chown www-data:www-data /etc/glpi/local_define.php
+sudo chmod 640 /etc/glpi/local_define.php
+
+
+ls -l /etc/glpi/
