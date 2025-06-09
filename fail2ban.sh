1f144e8adccdad368222c6c981e2bd1e56c24f68
Saho <147002941+Sahozen@users.noreply.github.com>
2025-06-10 01:37:10 +0200

    Create fail2ban.sh

---
+++ fail2ban.sh
@@ -0,0 +1,42 @@
+# Mise à jour des paquets
+sudo apt update && sudo apt upgrade -y
+sudo apt install cockpit -y
+
+sudo systemctl start cockpit
+sudo systemctl enable cockpit
+
+# Installation de fail2ban
+sudo apt install fail2ban -y
+
+
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+
+sudo nano /etc/fail2ban/jail.local
+
+
+[sshd]
+enabled = true
+port = ssh
+filter = sshd
+logpath = /var/log/auth.log
+maxretry = 3
+bantime = 600
+
+
+sudo systemctl restart fail2ban
+sudo systemctl enable fail2ban
+
+sudo apt install cockpit-fail2ban -y
+
+
+sudo systemctl restart cockpit
+
+# Voir les jails actives
+sudo fail2ban-client status
+
+# Voir les IP bannies pour sshd
+sudo fail2ban-client status sshd
+
+#pour deban
+sudo fail2ban-client set sshd unbanip <IP>
