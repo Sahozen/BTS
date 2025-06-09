7c6baaf3b81a5350c93ea7359d7900218ab7c03e
Saho <147002941+Sahozen@users.noreply.github.com>
2025-06-10 01:43:02 +0200

    Create depotDebian.sh

---
+++ depotDebian.sh
@@ -0,0 +1,5 @@
+sudo nano /etc/apt/sources.list
+
+deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
