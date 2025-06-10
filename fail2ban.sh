0ef006cfb307b2b01c9dd27d684e3125b10351e7
Saho <147002941+Sahozen@users.noreply.github.com>
2025-06-10 02:15:37 +0200

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -15,6 +15,14 @@ sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sudo nano /etc/fail2ban/jail.local
 
 
+
+sudo systemctl status rsyslog
+sudo apt install rsyslog -y
+sudo systemctl enable rsyslog
+sudo systemctl start rsyslog
+
+ls -l /var/log/auth.log
+
 [sshd]
 enabled = true
 port = ssh
