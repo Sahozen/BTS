84f8e6a9a443034cdfda5801bf7985585916b976
Saho <147002941+Sahozen@users.noreply.github.com>
2025-06-13 18:11:47 +0200

    Create modeleglpi.sh

---
+++ modeleglpi.sh
@@ -0,0 +1,37 @@
+Demande création utilisateur : 
+
+Bonjour,
+
+Merci de créer un compte pour le nouvel arrivant :
+
+- Nom / Prénom :
+- Service :
+- Logiciels nécessaires :
+- Accès réseau / partages :
+
+L'utilisateur rencontre un souci avec son poste :
+
+- Nom de l'utilisateur :
+- Numéro de poste ou PC :
+- Description du problème :
+- Action déjà tentée :
+
+
+Bonjour,
+
+L'utilisateur ne parvient pas à se connecter :
+
+- Type d'accès concerné : VPN / Internet / Partage réseau / Autre
+- Date et heure du problème :
+- Message d’erreur affiché :
+- Tentatives de résolution :
+
+
+Bonjour,
+
+Un incident majeur impacte l'activité :
+
+- Nombre de personnes concernées :
+- Service impacté :
+- Application ou matériel bloqué :
+- Tentatives de contournement :
